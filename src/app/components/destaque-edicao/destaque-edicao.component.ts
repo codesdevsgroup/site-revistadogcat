06dc73d25d915cef829cd1bf11ca423da8631af7
Lucas Campos <bhlucascampos@gmail.com>
2025-05-30 22:12:59 -0300

    ajuste inicial

---
+++ src/app/components/destaque-edicao/destaque-edicao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-destaque-edicao',
+  imports: [],
+  templateUrl: './destaque-edicao.component.html',
+  styleUrl: './destaque-edicao.component.scss'
+})
+export class DestaqueEdicaoComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import {DestaqueEdicaoComponent} from '../../components/destaque-edicao/destaque-edicao.component';
+import {FooterComponent} from '../../components/footer/footer.component';
+import {MetricsComponent} from '../../components/metrics/metrics.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    DestaqueEdicaoComponent,
+    FooterComponent,
+    MetricsComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  socialMedia = {
+    instagram: '@dogcatbr_instagram',
+    facebook: '@dogcatbr_facebook',
+    tiktok: '@dogcatbr_tiktok'
+  };
 }
