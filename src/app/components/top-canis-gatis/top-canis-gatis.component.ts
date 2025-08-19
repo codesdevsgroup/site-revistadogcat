2af3c25cfcb1ae43b56275712a0590136ae212b9
lucascampos42 <bhlucascampos@gmail.com>
2025-08-19 10:45:10 -0300

    Modal de anuncio

--- src/app/components/top-canis-gatis/top-canis-gatis.component.ts
+++ src/app/components/top-canis-gatis/top-canis-gatis.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { ModalCadastroAnuncianteComponent } from '../modal-cadastro-anunciante/modal-cadastro-anunciante.component';
 
 @Component({
   selector: 'app-top-canis-gatis',
-  imports: [],
+  imports: [ModalCadastroAnuncianteComponent],
   templateUrl: './top-canis-gatis.component.html',
   styleUrl: './top-canis-gatis.component.scss'
 })
 export class TopCanisGatisComponent {
+  isModalOpen = false;
 
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  onFormSubmit(formData: any) {
+    console.log('Dados do formulário:', formData);
+    // Aqui você pode integrar com um serviço para enviar os dados
+  }
 }
