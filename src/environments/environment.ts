14403bfce4354685300cd93fc8c092f705273284
lucascampos42 <bhlucascampos@gmail.com>
2025-12-15 23:28:53 -0300

    feat: adiciona páginas de administração para listar e gerenciar edições e configura variáveis de ambiente.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://api.revistadogcat.com.br',
   appName: 'Revista Dog & Cat - Dev',
   version: '1.0.0-dev',
   enableLogging: true,
